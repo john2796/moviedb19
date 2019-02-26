622f30b17ed14d0f7837ee4ae1325a9f9f537eee
john2796 <jbmiranda22796@gmail.com>
2019-02-26 14:36:44 -0800

    added comments header carousel

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Navbar from "../../components/navbar/Navbar";
+import { callbackify } from "util";
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        {/* Header Carousel */}
         <h1>etc</h1>
       </div>
     );
