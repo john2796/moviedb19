f91ea803638a2fa221d33536115a74254303a957
christiansallen <christiansallen@gmail.com>
2019-02-26 19:44:37 -0700

    Finished dummy data header carousel

--- src/components/header-carousel/HeaderCarousel.js
+++ src/components/header-carousel/HeaderCarousel.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import "./HeaderCarousel.css";
 
 class HeaderCarousel extends Component {
@@ -9,7 +11,49 @@ class HeaderCarousel extends Component {
   render() {
     return (
       <div className="header-carousel-container">
-        <h1>HeaderCarousel</h1>
+        <Carousel
+          autoPlay
+          interval="5000"
+          infiniteLoop
+          showThumbs={false}
+          showStatus={false}
+          showArrows={false}
+          emulateTouch={true}
+        >
+          <div className="carousel-slide">
+            <img
+              src="https://image.tmdb.org/t/p/w1280/7d6EY00g1c39SGZOoCJ5Py9nNth.jpg"
+              style={{ opacity: "0.6" }}
+            />
+
+            <div className="carousel-movie-info">
+              <h2 className="carousel-movie-title">
+                SPIDERMAN: INTO THE SPIDERVERSE
+              </h2>
+              <p>Animation | 8.1 Rating</p>
+            </div>
+          </div>
+          <div className="carousel-slide">
+            <img
+              src="https://image.tmdb.org/t/p/w1280/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg"
+              style={{ opacity: "0.6" }}
+            />
+            <div className="carousel-movie-info">
+              <h2 className="carousel-movie-title">GLASS</h2>
+              <p>Thriller | 7.7 Rating</p>
+            </div>
+          </div>
+          <div className="carousel-slide">
+            <img
+              src="https://image.tmdb.org/t/p/w1280/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg"
+              style={{ opacity: "0.6" }}
+            />
+            <div className="carousel-movie-info">
+              <h2 className="carousel-movie-title">AVENGERS: INFINITY WAR</h2>
+              <p>Action | 8.3 Rating</p>
+            </div>
+          </div>
+        </Carousel>
       </div>
     );
   }
