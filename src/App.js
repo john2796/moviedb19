73579b3a65375a62145ed0abfbe15bf0504a7cf5
john2796 <jbmiranda22796@gmail.com>
2019-02-28 21:06:20 -0800

    adding login functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import Register from "./containers/register/Register";
 import Login from "./containers/login/Login";
 import Dashboard from "./containers/dashboard/Dashboard";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* Routes dynamic */}
-        <Register />
-        <Login />
-        <Dashboard />
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </div>
     );
   }
