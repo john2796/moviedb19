ebbcfa98d059818719dc6b83f8a929ecc4eb8419
john2796 <jbmiranda22796@gmail.com>
2019-03-01 02:32:47 -0800

    completed upcoming style and image details

--- src/components/upcoming-carousel/UpcomingInfiniteCarousel.js
+++ src/components/upcoming-carousel/UpcomingInfiniteCarousel.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div style={{ ...style }} className="slick-arrow right" onClick={onClick}>
+      <i className="fas fa-chevron-right" />
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div style={{ ...style }} className="slick-arrow left" onClick={onClick}>
+      <i className="fas fa-chevron-left" />
+    </div>
+  );
+}
 class UpcomingInfiniteCarousel extends Component {
   render() {
     const { upcoming } = this.props;
@@ -9,8 +26,33 @@ class UpcomingInfiniteCarousel extends Component {
       centerMode: true,
       infinite: true,
       centerPadding: "60px",
-      slidesToShow: 5,
-      speed: 400
+      slidesToShow: 6,
+      speed: 400,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 4,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
     };
 
     return (
