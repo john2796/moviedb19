0bcf29a0bd8e793467f8fd026e6bea7e61f9547b
john2796 <jbmiranda22796@gmail.com>
2019-03-15 15:02:00 -0700

    adding style on header banner

--- src/components/individual-movie/IndividualMovie.js
+++ src/components/individual-movie/IndividualMovie.js
@@ -7,18 +7,17 @@ import { getUpcoming, getPopular, getNowPlaying, getTopRated, getGenres } from '
 
 class IndividualMovie extends Component {
   componentDidMount() {
-
     const { tab, getPopular, getUpcoming, getNowPlaying, getTopRated, getGenres } = this.props
     if (tab === 'upcoming') {
-      getUpcoming();
+      return getUpcoming();
     } else if (tab === 'popular') {
-      getPopular()
-    } else if (tab === 'popular') {
-      getNowPlaying()
+      return getPopular()
+    } else if (tab === 'nowPlaying') {
+      return getNowPlaying()
     } else if (tab === 'topRated') {
-      getTopRated()
+      return getTopRated()
     } else if (tab === 'genres') {
-      getGenres()
+      return getGenres()
     }
   }
 
@@ -29,11 +28,36 @@ class IndividualMovie extends Component {
     const arr = [upcoming, popular, nowPlaying, topRated, genres].filter(x => x.length > 0).flat(1)
     const id = parseInt(match.params.id)
     const movie = arr.find((item) => item.id === id);
-
+    console.log(movie)
     console.log({ upcoming, popular, nowPlaying, topRated, genres })
     return (
-      <div className="individual-movie-parent">
-        <h1>{movie && movie.title}</h1>
+      <div className="individual-movie-parent"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${movie && movie.backdrop_path})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          width: "100vw",
+          height: "100vh",
+          zIndex: '100',
+        }}
+      >
+        <div className="individual-movie-darken">
+          <div className="individual-movie-wrapper">
+
+            <div className="individual-movie-inner">
+              <div className="movie-controls-main">
+                <p>left</p>
+                <p>right</p>
+              </div>
+              <div className="movie-info-main">
+                <img src={`https://image.tmdb.org/t/p/w1280/${movie && movie.poster_path}`} alt="poster_path" />
+                <h1>{movie && movie.title}</h1>
+              </div>
+            </div>
+
+          </div>
+        </div>
 
       </div>
     );
